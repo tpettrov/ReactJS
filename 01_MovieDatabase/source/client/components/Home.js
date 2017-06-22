57228623d3c6e9a4711f917c89f346d170f841c6
tpettrov <tpettrov@gmail.com>
2017-06-23 00:25:54 +0300

    start

---
+++ 01_MovieDatabase/source/client/components/Home.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Toni on 6/22/2017.
+ */
+import React from 'react';
+
+export default class Home extends React.Component {
+
+
+    render() {
+        return (
+            <div>
+                And I am Home component
+            </div>
+        )
+    }
+}
